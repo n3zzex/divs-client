ea9ec7058c9a534f09a871a07c3200d5a7f75740
Snay1 <89585275+Snay1@users.noreply.github.com>
2023-12-25 13:39:39 +0300

    added yandex map

---
+++ src/js/contactsMap.js
@@ -0,0 +1,22 @@
+initMap();
+
+async function initMap() {
+    // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
+    await ymaps3.ready;
+
+    const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+
+    // Иницилиазируем карту
+    const map = new YMap(
+        document.getElementById('contactsMap'),
+
+        {
+            location: {
+                center: [37.588144, 55.733842],
+                zoom: 10
+            }
+        }
+    );
+
+    map.addChild(new YMapDefaultSchemeLayer());
+}
