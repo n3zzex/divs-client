5c7fc562abdfdca41ccdc1194b5db3d70c24ad32
Snay1 <89585275+Snay1@users.noreply.github.com>
2023-12-26 21:13:58 +0300

    all sliders added

--- src/js/contactsMap.js
+++ src/js/contactsMap.js
@@ -1,14 +1,19 @@
 initMap();
 
 async function initMap() {
-    // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
     await ymaps3.ready;
 
     const {YMap, YMapDefaultSchemeLayer} = ymaps3;
 
+    const mapEl = document.getElementById('contactsMap');
+
+    if (!mapEl) {
+        return;
+    }
+
     // Иницилиазируем карту
     const map = new YMap(
-        document.getElementById('contactsMap'),
+        mapEl,
 
         {
             location: {
--- src/js/slider.js
+++ src/js/slider.js
@@ -10,7 +10,7 @@ const mainSliderConfig = {
   centerMode: true,
 
   pagination: {
-      el: ".main-arena__pagination",
+      el: ".main-arena__slider .main-arena__pagination",
       type: "bullets",
       clickable: true,
     },
@@ -43,6 +43,215 @@ const sliderThreeItemsConfig = {
 }
 
 new Swiper(mainArenaSlider, mainSliderConfig);
+
+new Swiper(".events__slider .content", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centerMode: true,
+
+  pagination: {
+      el: ".events__pagination",
+      type: "bullets",
+      clickable: true,
+    },
+
+  navigation: {
+    nextEl: '.events-next',
+    prevEl: '.events-prev',
+  },
+
+  breakpoints: {
+    767: {
+      slidesPerView: 4,
+    }
+  }
+  
+});
+
+new Swiper(".gyms-dance__slider .content", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centerMode: true,
+
+  pagination: {
+      el: ".gyms-dance__pagination",
+      type: "bullets",
+      clickable: true,
+    },
+
+  navigation: {
+    nextEl: '.gyms-dance__next',
+    prevEl: '.gyms-dance__prev',
+  },
+
+  breakpoints: {
+    767: {
+      slidesPerView: 4,
+    }
+  }
+  
+});
+new Swiper(".gyms-workout__slider .content", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centerMode: true,
+
+  pagination: {
+      el: ".gyms-workout__pagination",
+      type: "bullets",
+      clickable: true,
+    },
+
+  navigation: {
+    nextEl: '.gyms-workout__next',
+    prevEl: '.gyms-workout__prev',
+  },
+
+  breakpoints: {
+    767: {
+      slidesPerView: 4,
+    }
+  }
+  
+});
+new Swiper(".rent-press__slider .content", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centerMode: true,
+
+  pagination: {
+      el: ".rent-press__pagination",
+      type: "bullets",
+      clickable: true,
+    },
+
+  navigation: {
+    nextEl: '.rent-press__next',
+    prevEl: '.rent-press__prev',
+  },
+
+  breakpoints: {
+    767: {
+      slidesPerView: 4,
+    }
+  }
+  
+});
+new Swiper(".rent-vip__slider .content", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centerMode: true,
+
+  pagination: {
+      el: ".rent-vip__pagination",
+      type: "bullets",
+      clickable: true,
+    },
+
+  navigation: {
+    nextEl: '.rent-vip__next',
+    prevEl: '.rent-vip__prev',
+  },
+
+  breakpoints: {
+    767: {
+      slidesPerView: 4,
+    }
+  }
+  
+});
+new Swiper(".rent-arena__slider .content", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centerMode: true,
+
+  pagination: {
+      el: ".rent-arena__pagination",
+      type: "bullets",
+      clickable: true,
+    },
+
+  navigation: {
+    nextEl: '.rent-arena__next',
+    prevEl: '.rent-arena__prev',
+  },
+
+  breakpoints: {
+    767: {
+      slidesPerView: 4,
+    }
+  }
+  
+});
+new Swiper(".rent-area__slider .content", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centerMode: true,
+
+  pagination: {
+      el: ".rent-area__pagination",
+      type: "bullets",
+      clickable: true,
+    },
+
+  navigation: {
+    nextEl: '.rent-area__next',
+    prevEl: '.rent-area__prev',
+  },
+
+  breakpoints: {
+    767: {
+      slidesPerView: 4,
+    }
+  }
+  
+});
+new Swiper(".rent-floor__slider .content", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centerMode: true,
+
+  pagination: {
+      el: ".rent-floor__pagination",
+      type: "bullets",
+      clickable: true,
+    },
+
+  navigation: {
+    nextEl: '.rent-floor__next',
+    prevEl: '.rent-floor__prev',
+  },
+
+  breakpoints: {
+    767: {
+      slidesPerView: 4,
+    }
+  }
+  
+});
+new Swiper(".gyms-physical__slider .content", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centerMode: true,
+
+  pagination: {
+      el: ".gyms-physical__pagination",
+      type: "bullets",
+      clickable: true,
+    },
+
+  navigation: {
+    nextEl: '.gyms-physical__next',
+    prevEl: '.gyms-physical__prev',
+  },
+
+  breakpoints: {
+    767: {
+      slidesPerView: 4,
+    }
+  }
+  
+});
 new Swiper(newsSlider, {
   ...sliderThreeItemsConfig,
   pagination: {
@@ -56,6 +265,23 @@ new Swiper(newsSlider, {
     prevEl: '.news-prev',
   },
 });
+
+new Swiper(".hotel-slider__slider", {
+  ...sliderThreeItemsConfig,
+  pagination: {
+      el: ".hotel-page-slider-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+
+  spaceBetween: 20,
+
+  navigation: {
+    nextEl: '.hotel-slider-next',
+    prevEl: '.hotel-slider-prev',
+  },
+});
+
 new Swiper(lettersSlider, {
   ...sliderThreeItemsConfig,
   pagination: {
@@ -79,4 +305,57 @@ new Swiper('.concert-slider', {
     prevEl: '.concert__prev',
   },
 });
+new Swiper('.hotel .hotel-slider-wrapper', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centerMode: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 4,
+    }
+  },
+  pagination: {
+    el: ".hotel-slider-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.hotel-next',
+    prevEl: '.hotel-prev',
+  },
+});
+
+new Swiper('.concert-slider', {
+  slidesPerView: 1,
+  loop: true,
+
+  navigation: {
+    nextEl: '.concert__next',
+    prevEl: '.concert__prev',
+  },
+});
+
+
+new Swiper('.reviews__slider', {
+  slidesPerView: 2,
+  loop: true,
+  spaceBetween: 20,
+
+  navigation: {
+    nextEl: '.fitness-reviews__next',
+    prevEl: '.fitness-reviews__prev',
+  },
+
+  pagination: {
+    el: ".main-arena__pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  
+});
+
 
