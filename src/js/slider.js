4f9f4168dcf78844f8bd6836a26bc0ceed921edb
Snay1 <89585275+Snay1@users.noreply.github.com>
2023-12-26 16:00:52 +0300

    added some sliders

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,5 +1,7 @@
 
 const mainArenaSlider = document.querySelector(".main-arena__slider .content");
+const newsSlider = document.querySelector(".news-slider");
+const lettersSlider = document.querySelector(".letters-slider-wrapper");
 
 
 const mainSliderConfig = {
@@ -26,4 +28,55 @@ const mainSliderConfig = {
   
 }
 
+const sliderThreeItemsConfig = {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centerMode: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    }
+  }
+}
+
 new Swiper(mainArenaSlider, mainSliderConfig);
+new Swiper(newsSlider, {
+  ...sliderThreeItemsConfig,
+  pagination: {
+      el: ".news-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+
+  navigation: {
+    nextEl: '.news-next',
+    prevEl: '.news-prev',
+  },
+});
+new Swiper(lettersSlider, {
+  ...sliderThreeItemsConfig,
+  pagination: {
+      el: ".letters-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+
+  navigation: {
+    nextEl: '.letters-next',
+    prevEl: '.letters-prev',
+  },
+});
+
+new Swiper('.concert-slider', {
+  slidesPerView: 1,
+  loop: true,
+
+  navigation: {
+    nextEl: '.concert__next',
+    prevEl: '.concert__prev',
+  },
+});
+
